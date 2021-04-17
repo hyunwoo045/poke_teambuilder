804ff5e378ab15460b5ba32574229351d87f67e4
yeondoo <hyunwoo045@gmail.com>
2021-04-17 17:33:32 +0900

    getting started with git

---
+++ app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const session = require('express-session');
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
+const server = app.listen(3000, function() {
+    console.log("Express server has started on port 3000");
+});
+app.use(express.static('public'));
+app.use(express.static('dist'));
+app.use(session({
+    secret: '!@#$$#@!',
+    resave: false,
+    saveUninitialized: true
+}));
+var router = require('./router/main.js')(app, fs);
